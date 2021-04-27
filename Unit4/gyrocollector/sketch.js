01a833edf7507d8d6b74dade3fb9e3add8d420db
gshafer-art <77454374+gshafer-art@users.noreply.github.com>
2021-04-27 11:16:00 -0500

    we

--- Unit4/gyrocollector/sketch.js
+++ Unit4/gyrocollector/sketch.js
@@ -174,23 +174,24 @@ function Car() {
 }
 
 function mush(){
-  translate (0, -50);
-  fill ('white');
+  fill('black');
+  translate(0, -50);
+  fill('white');
   quad(100, 310, 150, 340, 150, 360, 100, 330);
   quad(300, 310, 250, 340, 250, 360, 300, 330);
-  rect (150, 300, 100, 100);
-  rect (150, 400, 20, 50);
-  rect (230, 400, 20, 50);
-  fill ('red');
-  ellipse (200, 250, 200, 120);
-  fill ('white');
-  ellipse (140, 250, 30, 30);
-  ellipse (230, 230, 30, 30);
-  ellipse (175, 210, 30, 30);
-  ellipse (270, 270, 30, 30);
-  ellipse (200, 280, 30, 30);
-  fill ('black');
-  ellipse (170, 330, 10, 10);
-  ellipse (230, 330, 10, 10);
-  ellipse (200, 350, 20, 20);
+  rect(160, 420, 20, 50);
+  rect(240, 420, 20, 50);
+  rect(200, 350, 100, 100);
+  fill('red');
+  ellipse(200, 250, 200, 120);
+  fill('white');
+  ellipse(140, 250, 30, 30);
+  ellipse(230, 230, 30, 30);
+  ellipse(175, 210, 30, 30);
+  ellipse(270, 270, 30, 30);
+  ellipse(200, 280, 30, 30);
+  fill('black');
+  ellipse(170, 330, 10, 10);
+  ellipse(230, 330, 10, 10);
+  ellipse(200, 350, 20, 20);
 }
